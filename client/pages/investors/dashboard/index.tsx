c51fbce0d10d5cd4dda8b0c557056f851c4f248c
Iam-nishanth <nishanthalapati123@gmail.com>
2023-11-11 13:48:55 +0530

    Build Fixes

--- client/pages/investors/dashboard/index.tsx
+++ client/pages/investors/dashboard/index.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import Navbar from '@/components/Header/Navbar';
-import Sidebar from '@/components/Header/Sidebar';
 import { useAuthContext } from '@/hooks/useAuthContext';
 import { message } from 'antd';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
 import InvestorAuth from '@/components/HighOrders/Investor';
 import InvestorDash from '@/views/InvestorDash';
-import Layout from '@/components/Layout';
 import BackButton from '@/components/BackButton';
 
 const InvestorDashboard = () => {
